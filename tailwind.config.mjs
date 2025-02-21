fa47a84fac5a51b6d8a60f699b358ce09c960175
asmtraders4u <asmtraders4u@gmail.com>
2025-02-21 12:12:03 +0000

    fixed api changes in cateogry navbar

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -16,7 +16,16 @@ export default {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
-  		}
+  		},
+		  animation: {
+			"scroll-left": "scroll-left 40s linear infinite",
+		  },
+		  keyframes: {
+			"scroll-left": {
+			  "0%": { transform: "translateX(100%)" },
+			  "100%": { transform: "translateX(-100%)" },
+			},
+		  },
   	}
   },
   plugins: [require("tailwindcss-animate")],
