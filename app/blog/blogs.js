b2e84ba7770c340a947b46f35728ded6f9f8ab11
Mohd Muneeb Khan <113891294+muneeb-rahmani@users.noreply.github.com>
2025-04-06 16:54:29 +0530

    feat: refactor product and category data handling with new API structure; update components for improved data flow and readability

--- app/blog/blogs.js
+++ app/blog/blogs.js
@@ -12,7 +12,7 @@ const BlogPage = ({serverData}) => {
       <h1 className="text-3xl font-bold text-center mb-6">Latest Blogs</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {serverData.map((blog, index) => {
-          const formattedUrl = getSlug(blog.url)
+          const formattedUrl = getSlug(blog.url, 'split')
           return (
           <div key={index} className="border rounded-lg overflow-hidden shadow-lg">
             <div className="p-4">
@@ -24,7 +24,7 @@ const BlogPage = ({serverData}) => {
                   height={200}
                 />
               </Link>
-              <h2 className="text-xl font-semibold">
+              <h2 className="text-xl font-semibold mt-4">
                 <Link href={formattedUrl} className="blogTitle">
                   {blog.title}
                 </Link>
@@ -33,7 +33,7 @@ const BlogPage = ({serverData}) => {
                 {new Date(blog.date).toLocaleDateString()}
               </p> */}
               {/* <p className="text-gray-800 mt-2" dangerouslySetInnerHTML={{ __html: blog.excerpt.rendered }}></p> */}
-              <Link href={`/blog/${formattedUrl}`} className="text-red-500 font-semibold mt-4 inline-block">
+              <Link href={formattedUrl} className="text-red-500 font-semibold mt-4 inline-block">
                 Read More →
               </Link>
             </div>
--- app/home/page.js
+++ app/home/page.js
@@ -13,50 +13,16 @@ import axios from "axios";
 import { parseRankMathData } from "../utils/common";
 
 const Page = async () => {
-  // const productData = await getProducts();
-  // console.log(productData, 'check muneeb')
+
   const homeData = await getHomeData()
-  console.log(homeData, 'check muneeb')
-
-  const categoryIds = await getCategories();
-  const fetchCategoryAndProducts = async () => {
-    try {
-      const categoryId = categoryIds?.map(({ id: itemId, name: itemName }) => ({
-        id: itemId,
-        name: itemName,
-      }));
-      // console.log(categoryId, "check categoryId data");
-      const data = await axios?.all(
-        categoryId?.map(async (item) => {
-          // console.log(item.id, "check id from categoryId map");
-          const products = await getProductsByCategory(item.id);
-          return { products, category: item.name };
-        })
-      );
-      // console.log(data, "check category data");
-  
-      return data;
-      
-    } catch (error) {
-      console.log("Error in  fetchCategoryAndProducts", error);
-      throw error
-    }
-  };
-
-  const homepageDescripton = await getPagesFromCustom("homepage")
-  // console.log(homepageDescripton , 'check homepageDescripton')
-  const url = process.env.NEXT_PUBLIC_BASE_URL;
-  const data = await fetchCategoryAndProducts();
-  const rankMathData = await getSEOData(url)
-  const jsonLdData = parseRankMathData(rankMathData);
 
   return (
     <>
-      <HomePage productData={data} homeData={homeData} homepageDescripton={homepageDescripton} />
+      <HomePage  homeData={homeData} />
       <script
         type="application/ld+json"
         dangerouslySetInnerHTML={{
-          __html: JSON.stringify(jsonLdData, null, 2),
+          __html: JSON.stringify(homeData?.schema_data),
         }}
       />
     </>
